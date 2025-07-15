9682bbdb32f2683931d17546d1ba38859b9faa01
alex <cikosalex@gmail.com>
2025-07-15 19:51:35 +0930

    Getting app ready for deployment

---
+++ server.js
@@ -0,0 +1,10 @@
+import { handler } from './.svelte-kit/output/server/index.js';
+import polka from 'polka';
+
+const port = process.env.PORT || 3000;
+
+polka()
+  .use(handler)
+  .listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
